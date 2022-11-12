15f0aa3373fe97f4d84165495ede0b5ed9c2cfa0
ASHPAK RAKEEB MOHAMMAD <as449248@dal.ca>
2022-11-12 18:43:12 -0400

    User management module

---
+++ halifax_foodie/src/UserPool.js
@@ -0,0 +1,8 @@
+import { CognitoUserPool } from "amazon-cognito-identity-js"
+
+const poolData = {
+    UserPoolId: "us-east-1_P4ERbyxCu",
+    ClientId: "70f6652uitkaepqklouaupe68q",
+};
+
+export default new CognitoUserPool(poolData);
