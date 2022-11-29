6eb3ab3bbfeeead54cf88df5b50f2e0ac7131b5b
Sujeet Kc <sujeetkc45@gmail.com>
2022-11-29 16:40:18 -0400

    visualization error fix

--- halifax_foodie/src/components/visualization/Visualization.jsx
+++ halifax_foodie/src/components/visualization/Visualization.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tabs, Tab } from "react-bootstrap";
 
 import './Visualization.scss'
 
 export default function Visualization() {
+    const [key, setKey] = useState('login');
+
     return (
         <div className="viz-container row justify-content-center align-items-center">
             <div className="col-md-10">
@@ -11,14 +13,22 @@ export default function Visualization() {
                 {/* https://react-bootstrap.github.io/components/tabs/ */}
                 <Tabs
                     defaultActiveKey="login"
+                    activeKey={key}
                     id="uncontrolled-tab-example"
+                    onSelect={(k) => setKey(k)}
                     fill
                 >
                 <Tab eventKey="login" title="Login Visualization">
-                    <iframe title="Login Visualization" width="100%" height="450" src="https://datastudio.google.com/embed/reporting/b60cab91-386d-47aa-87f6-2a9c90f46bbb/page/G0t8C" frameborder="0" style={{border:0}} allowfullscreen></iframe>
+                    {
+                        key === 'login' &&
+                        <iframe title="Login Visualization" width="100%" height="450" src="https://datastudio.google.com/embed/reporting/b60cab91-386d-47aa-87f6-2a9c90f46bbb/page/G0t8C" frameborder="0" style={{border:0}} allowfullscreen></iframe>
+                    }
                 </Tab>
                 <Tab eventKey="recipe" title="Recipe Visualization">
-                <iframe title="Recipe Visualization" width="100%" height="450" src="https://datastudio.google.com/embed/reporting/e67fea71-294f-4dac-9b92-94c4d1b7fac9/page/YDu8C" frameborder="0" style={{border:0}} allowfullscreen></iframe>
+                    {
+                        key === 'recipe' &&
+                        <iframe title="Recipe Visualization" width="100%" height="450" src="https://datastudio.google.com/embed/reporting/e67fea71-294f-4dac-9b92-94c4d1b7fac9/page/YDu8C" frameborder="0" style={{border:0}} allowfullscreen></iframe>
+                    }
                 </Tab>
                 </Tabs>
             </div>
