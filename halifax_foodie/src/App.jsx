4e33bdcca4f014fb400ee7bb7926eae11c33db61
Sujeet Kc <sujeetkc45@gmail.com>
2022-11-29 17:20:28 -0400

    app.jsx refactoring

--- halifax_foodie/src/App.jsx
+++ halifax_foodie/src/App.jsx
@@ -1,34 +1,28 @@
 import React from "react";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, NavLink, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
 import Order from "./components/order/orderPage";
 import RealTimeChat from './components/real-time-chat/RealTimeChat';
 import RecipeUpload from "./components/recipeUpload/RecipeUpload";
 import giveRating from "./components/review/giveRating";
 
-import { useHistory } from 'react-router-dom';
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
 
-import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import "../src/main.css";
 import './App.scss';
 import MultiFactor from "./components/mfa/Mfa";
 import Visualization from "./components/visualization/Visualization";
 
 function App() {
-  const history = useHistory();
+
   const logout = () => {
     localStorage.clear();
     window.location.reload();
-    history.push("/");
   };
-  const home= () => {
-    history.push("/");
-    window.location.reload();
 
-  };
   let isQuestion = !!JSON.parse(localStorage.getItem("IsQuestion"))
 
   return (
@@ -36,46 +30,32 @@ function App() {
 
       {/* {//references: */}
       {/* //https://mui.com/material-ui/react-app-bar/} */}
-
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-          onClick={() => home()}
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Halifax Foodie
-          </Typography>
-          <Button color="inherit" onClick={() => logout()}>Logout</Button>
-        </Toolbar>
-      </AppBar>
-
-      {!isQuestion ? (
         <Router>
-          <Switch>
-            <Route exact path="/" component={MultiFactor} />
-          </Switch>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <NavLink style={{textDecoration: 'none', color: 'white', cursor: 'pointer'}} to={'/'}> Halifax Foodie</NavLink>
+              </Typography>
+              <Button color="inherit" onClick={() => logout()}>Logout</Button>
+            </Toolbar>
+          </AppBar>
+          {
+            isQuestion ?
+            <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/question" component={MultiFactor} />
+                <Route exact path="/giveratings" component={giveRating} />
+                <Route exact path="/chat" component={RealTimeChat} />
+                <Route exact path="/order" component={Order} />
+                <Route exact path="/recipeupload" component={RecipeUpload} />
+                <Route exact path="/visualization" component={Visualization} />
+            </Switch>
+            :
+            <Switch>
+              <Route exact path="/" component={MultiFactor} />
+            </Switch>
+          }
         </Router>
-      ) : (
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route exact path="/question" component={MultiFactor} />
-            <Route exact path="/giveratings" component={giveRating} />
-            <Route exact path="/chat" component={RealTimeChat} />
-            <Route exact path="/order" component={Order} />
-            <Route exact path="/recipeupload" component={RecipeUpload} />
-            <Route exact path="/visualization" component={Visualization} />
-          </Switch>
-        </Router>
-      )}
-  {
-  }
     </Box>
 
   );
