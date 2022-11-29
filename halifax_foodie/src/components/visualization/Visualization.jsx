0df6750b3cb8ddede1349d4b2b5bf5e89409e7a4
Sujeet Kc <sujeetkc45@gmail.com>
2022-11-29 16:44:04 -0400

    visualization box height fix

--- halifax_foodie/src/components/visualization/Visualization.jsx
+++ halifax_foodie/src/components/visualization/Visualization.jsx
@@ -7,7 +7,7 @@ export default function Visualization() {
     const [key, setKey] = useState('login');
 
     return (
-        <div className="viz-container row justify-content-center align-items-center">
+        <div className="viz-container row justify-content-center align-items-center" style={{marginTop: "25px"}}>
             <div className="col-md-10">
                 {/* references */}
                 {/* https://react-bootstrap.github.io/components/tabs/ */}
@@ -21,13 +21,13 @@ export default function Visualization() {
                 <Tab eventKey="login" title="Login Visualization">
                     {
                         key === 'login' &&
-                        <iframe title="Login Visualization" width="100%" height="450" src="https://datastudio.google.com/embed/reporting/b60cab91-386d-47aa-87f6-2a9c90f46bbb/page/G0t8C" frameborder="0" style={{border:0}} allowfullscreen></iframe>
+                        <iframe title="Login Visualization" width="100%" height="600" src="https://datastudio.google.com/embed/reporting/b60cab91-386d-47aa-87f6-2a9c90f46bbb/page/G0t8C" frameborder="0" style={{border:0}} allowfullscreen></iframe>
                     }
                 </Tab>
                 <Tab eventKey="recipe" title="Recipe Visualization">
                     {
                         key === 'recipe' &&
-                        <iframe title="Recipe Visualization" width="100%" height="450" src="https://datastudio.google.com/embed/reporting/e67fea71-294f-4dac-9b92-94c4d1b7fac9/page/YDu8C" frameborder="0" style={{border:0}} allowfullscreen></iframe>
+                        <iframe title="Recipe Visualization" width="100%" height="600" src="https://datastudio.google.com/embed/reporting/e67fea71-294f-4dac-9b92-94c4d1b7fac9/page/YDu8C" frameborder="0" style={{border:0}} allowfullscreen></iframe>
                     }
                 </Tab>
                 </Tabs>
