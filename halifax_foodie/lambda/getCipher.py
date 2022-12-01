b5929d6b710fdea5cde7c9a98a215ee08d8b0561
Sanika Tamhankar <sn295037@dal.ca>
2022-12-01 14:49:00 -0400

    Dashboard changes

--- halifax_foodie/lambda/getCipher.py
+++ halifax_foodie/lambda/getCipher.py
@@ -4,38 +4,8 @@ import boto3
 from boto3.dynamodb.conditions import Attr
 
 dynamodb = boto3.resource('dynamodb')
-
-def decryptionCipher(cipher,key):
-    plainText = ""
-    k_indx = 0
-    plainText_index = 0
-    plainText_len = float(len(cipher))
-    plainText_lst = list(cipher)
-    col = len(key)    
-    row = int(math.ceil(plainText_len / col))
-    key_list = sorted(list(key))
-    dec_cipher = []
-    for _ in range(row):
-        dec_cipher += [[None] * col]
-    for _ in range(col):
-        curr_idx = key.index(key_list[k_indx])
-        for j in range(row):
-            dec_cipher[j][curr_idx] = plainText_lst[plainText_index]
-            plainText_index += 1
-        k_indx += 1
-    try:
-        plainText = ''.join(sum(dec_cipher, []))
-    except TypeError:
-        raise TypeError("This program cannot",
-                        "handle repeating words.")
-    null_count = plainText.count('_')
-    if null_count > 0:
-        return plainText[: -null_count]
-    print(plainText)
-    return plainText
     
 def lambda_handler(event, context):
-    # ev = json.loads(event['body'])
     ev=event
     username = ev['username']
     cipher =  ev['cipher']
@@ -48,13 +18,11 @@ def lambda_handler(event, context):
     print(data)
     for item in userData:
         if item['userID']== username:
-            generatedPlainText = decryptionCipher(cipher,item['userkey'])
-            print(item['plainText'])
-            print(generatedPlainText)
-            if item['plainText'] == generatedPlainText:
-                message = 'Verification successful'
+            if item['cipher'] == cipher:
+                message = 'User Verified'
             else:
-                message = 'User not verified'    
+                message = 'User Not Verified'
+   
     response = [{'message':message}]
     return {
         'statusCode':200 ,
